f267f971a0e0684737f9bb28f681cebe8838c95b
Hadar Sharon <hadar.sharon94@gmail.com>
2023-09-17 00:04:13 +0300

    speech model docstring

--- detectors/speech.py
+++ detectors/speech.py
@@ -5,6 +5,15 @@ from utils.config import Paths
 
 
 class ECAPA_TDNN:  # noinspection PyPep8Naming
+    """
+    Spoken language recognition model trained on the VoxLingua107 dataset using SpeechBrain.
+    The model uses the ECAPA-TDNN architecture that has previously been used for speaker recognition.
+    However, it uses more fully connected hidden layers after the embedding layer,
+    and cross-entropy loss was used for training.
+
+    On top of it, it leverages the speech_recognition Python library, which performs speech recognition,
+    with support for several engines and APIs, online and offline - purely for transcription purposes.
+    """
     MODEL_SOURCE = "speechbrain/lang-id-voxlingua107-ecapa"
     MODEL_DIRECTORY = Paths.MODELS / "speech/ecapa_tdnn"
 
