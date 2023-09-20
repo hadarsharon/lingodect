1d0805f251573fc6dfa08ec43c2c1393f2d75bb0
Hadar Sharon <hadar.sharon94@gmail.com>
2023-09-20 20:08:31 +0300

    more testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 from werkzeug.datastructures import FileStorage
 
-from app import get_language_name, process_text
+from app import get_language_name, process_text, process_audio
 from utils.config import Paths
 
 
@@ -35,4 +35,17 @@ def test_process_text_input():
 def test_process_text_file(file_input, expected):
     with open(file_input, 'rb') as f:
         prediction = process_text(file_input=FileStorage(f), multi_language=False)
+        assert prediction and prediction.language_codes and prediction.language_codes[0] == expected
+
+
+@pytest.mark.parametrize(
+    ("file_input", "expected"),
+    [(file_path, language_code) for file_path, language_code in zip(
+        glob.glob(str(Paths.TESTS / "test_inputs/speech/*.wav")),
+        [Path(file).stem for file in glob.glob(str(Paths.TESTS / "test_inputs/speech/*.wav"))]
+    )]
+)
+def test_process_audio_file(file_input, expected):
+    with open(file_input, 'rb') as f:
+        prediction = process_audio(file_input=FileStorage(f))
         assert prediction and prediction.language_codes and prediction.language_codes[0] == expected
